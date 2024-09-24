fb4b506e4eea5229957a12835fa229c638f15a68
Shashank Thirumals <shashank.thirumale@gmail.com>
2024-09-24 15:21:10 -0400

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import './Navbar.css'
+
 function Navbar() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener(("scroll"), () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   let logo =
     "https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940";
+  let avatar = "https://commons.wikimedia.org/wiki/File:Netflix-avatar.png"
 
   return (
-    <div className="nav">
-      <img className="nav_logo" src={logo} alt="" />
+    <div className={`nav ${show && "nav_black"}`}>
+      <img className="nav_logo" src={logo} alt="Netflix Logo" />
+      <img className="nav_avatar" src={avatar} alt="Avatar" />
     </div>
   );
 }
